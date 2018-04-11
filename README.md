7bf467c0abe0bcf2a81b4b3175295d13692bc932
Rahul Bhooteshwar <rahul.bhooteshwar@mastercard.com>
2018-04-11 10:06:50 +0530

    Minimal setup for webpack configuration.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # learn-webpack
 Sample project to practice webpack concepts
+
+## Current Issue
+
+Trying to follow instructions given in [Official Guide](https://webpack.js.org/guides/development/#using-webpack-dev-middleware) to setup webpack project using `webpack-dev-middleware`, but it is having problem while setting up path value for `output`.
+
+I am getting 404 on localhost:3000 while running dev server using command `npm run server`.
+
+Please refer the configuration used in `webpack.config.js` file of this project.
+
+What I observed is that when I set path value of configuration same as `publicPath` used for `webpack-dev-middleware` (see comment in `server.js`) It works fine.
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
+const app = express();
+const config = require('./webpack.config.js');
+// Without uncommenting following line it gives 404 error on localhost:3000
+//config.output.path = config.output.publicPath;
+const compiler = webpack(config);
+var path = require('path');
+// Tell express to use the webpack-dev-middleware and use the webpack.config.js
+// configuration file as a base.
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath
+}));
+
+// Serve the files on port 3000.
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!\n');
+});
